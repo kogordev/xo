05aa38c15c33aba78a09cb95a50644e9cb194cbb
kogor <kogordev@gmail.com>
2023-05-05 01:00:54 +0300

    try app response to screen change

--- src/components/PinkButton.vue
+++ src/components/PinkButton.vue
@@ -2,7 +2,7 @@
 	<div>
 		<button
 			type="button"
-			class="w-[5.5rem] sm:w-[7.2rem] sm:text-sm text-[.54rem] text-white bg-pink-700 hover:bg-pink-800 focus:ring-4 focus:ring-pink-300 font-medium rounded-lg px-5 py-2.5 mb-8 mx-1 dark:bg-pink-600 dark:hover:bg-pink-700 focus:outline-none dark:focus:ring-pink-800"
+			class="w-[5.5rem] sm:w-[7.2rem] sm:text-sm text-[.5rem] text-white bg-pink-700 hover:bg-pink-800 focus:ring-4 focus:ring-pink-300 font-medium rounded-lg px-5 py-2.5 mb-8 mx-1 dark:bg-pink-600 dark:hover:bg-pink-700 focus:outline-none dark:focus:ring-pink-800"
 			@click="$emit('NewGame')"
 		>
 			New Game
