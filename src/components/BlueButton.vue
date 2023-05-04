fcf018c3ce85cfdb852b400c6b0352a5df07c245
kogor <kogordev@gmail.com>
2023-05-05 01:31:07 +0300

    try app response to screen change

--- src/components/BlueButton.vue
+++ src/components/BlueButton.vue
@@ -2,7 +2,7 @@
 	<div>
 		<button
 			type="button"
-			class="w-[5.5rem] sm:w-[7.2rem] sm:text-sm smobile:text-[.57rem] text-[.35rem] text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg px-5 py-2.5 mb-8 mx-1 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+			class="w-[5.5rem] sm:w-[7.2rem] sm:text-sm smobile:text-[.5rem] text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg px-5 py-2.5 mb-8 mx-1 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
 			@click="$emit('ClearBoard')"
 		>
 			Clear Board
